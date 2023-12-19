1397b358811564d24c3686baf9ea71465a7872e6
letri2604 <let56800@gmail.com>
2023-12-19 21:56:17 +0700

    xong phần liên hệ

--- views/contact.view.php
+++ views/contact.view.php
@@ -4,11 +4,61 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $contact['title']; ?></title>
+    <link rel="stylesheet" href="../assets/css/index.css">
+    <link rel="stylesheet" href="../assets/css/post.css">
+    <title>Document</title>
+    <script src="../vendor/jquery-3.6.0.min.js"></script>
+    <script src="../vendor/popper.min.js"></script>
+    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="../vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
 </head>
 
 <body>
-    <?php echo $contact['content']; ?>
+    <div class="header">
+        <?php
+        include './includes/topbar.php';
+        include './includes/navbar.php';
+        ?>
+    </div>
+
+    <div class="content container">
+        <div class="row">
+            <div class="left-content col-md-9">
+                <br>
+                <p>Xin chào bạn – độc giả,</p>
+                <p>Lời đầu tiên, cho phép được gửi tới quý khách, quý thân nhân và bạn bè lời chúc sức khỏe, thành đạt
+                    và hạnh phúc.</p>
+                <p>Website được thành lập tháng 11/2011 dưới dạng Blog cá nhân, với mục đích ban đầu là nơi lưu trữ
+                    những <strong>bài viết kiến thức</strong> hoặc những <strong>sản phẩm sáng tạo</strong> trong quá
+                    trình học tập, làm việc. </p>
+                <p>Sau một thời gian hoạt động, website đã có những bước tiến đáng kể cả về chất lượng và hình thức. Số
+                    lượng khách truy cập website trở thành khách hàng sử dụng dịch vụ thiết kế tăng lên đáng kể. Từ việc
+                    chăm chút nội dung cho website theo sở thích, website đã dần chinh phục khách hàng với những dịch vụ
+                    thiết kế sáng tạo uy tín và chất lượng.</p>
+                <p>Tự tin với khả năng đáp ứng mọi yêu cầu thiết kế của khách hàng, chúng tôi chính thức đem lại dịch vụ
+                    <strong>Thiết kế đồ họa</strong> và <strong>Thiết kế Website chuyên nghiệp</strong>từ đầu năm 2017.
+                    Sự ủng hộ và tin tưởng của khách hàng sẽ trở thành nguồn cảm hứng lớn lao cho chúng tôi trong quá
+                    trình phác họa những ý tưởng thiết kế được trở thành hiện thực.</p>
+                <p>Xin cám ơn sự tin tưởng và ủng hộ của quý khách trong thời gian qua.</p>
+                <p>Trân trọng./.</p>
+                <p>Admin</p>
+            </div>
+
+            
+            <?php
+            include './includes/sidebar-post.php';
+            ?>
+
+        </div>
+    </div>
+    <!-- Footer -->
+    <?php
+            include './includes/footer.php';
+        ?>
 </body>
 
 </html>
